3f45197aa784e3c0de1b45f354970f3587cdef5a
Andrew Comer <andrew.comer@kratosdefense.com>
2015-12-03 14:35:36 -0500

    ..

--- src/com/FilamentTracker/Main.java
+++ src/com/FilamentTracker/Main.java
@@ -1,11 +1,6 @@
 /*
-<<<<<<< HEAD
- * ****STUFF TO DO*****
- * remove print/filament - using popup menu
-=======
  * ***STUFF TO DO****
  * remove print/filament - using popup menu..
->>>>>>> 06ae655fa681852148fc87121c9619702b237bfa
  * about dialog
  * more menu items
  * program icon
